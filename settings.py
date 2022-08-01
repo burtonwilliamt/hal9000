6d4b1b519355a96df6442609c3afe66308301c1e
William Burton <13026711+burtonwilliamt@users.noreply.github.com>
2022-07-31 21:17:35 -0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,2 +1,7 @@
+"""Global constants to control the bot functionality.
+
+Many of the values in this file are secret, substitute the
+correct values for your usage.
+"""
 BOT_TOKEN = 'REDACTED_TOKEN'
 GUILD_ID = 'REDACTED_GUILD_ID'
